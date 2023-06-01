2ba27994d96fd234c603bc3d5495ec71e905194b
echerkaoui1 <125316839+echerkaoui1@users.noreply.github.com>
2023-06-01 16:05:38 +0100

    Create script.js

---
+++ harder/script.js
@@ -0,0 +1,14 @@
+(function (window) {
+  var names = ["John", "Jane", "Jack", "Jill", "Jim"];
+
+  for (var i = 0; i < names.length; i++) {
+    var name = names[i];
+    var firstLetter = name.charAt(0).toLowerCase();
+
+    if (firstLetter === "j") {
+      byeSpeaker.speak(name);
+    } else {
+      helloSpeaker.speak(name);
+    }
+  }
+})(window);
